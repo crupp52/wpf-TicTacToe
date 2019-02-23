bf7ae406d50c9dbe0a46330ceda3bd1ad2df3ff5
ZUCKLEVENTE-PC\ASUS <zuck.levente@protonmail.com>
2019-02-23 13:13:12 +0100

    WinCheck is done

--- wfp-TicTacToe/wfp-TicTacToe/MainWindow.xaml.cs
+++ wfp-TicTacToe/wfp-TicTacToe/MainWindow.xaml.cs
@@ -67,7 +67,110 @@ namespace wfp_TicTacToe
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (mGameEnded)
+            {
+                NewGame();
+                return;
+            }
 
+            var button = (Button)sender;
+
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            int index = column + (row * 3);
+
+            if (mResults[index] != MarkType.Free)
+            {
+                return;
+            }
+
+            mResults[index] = mPlayer1Turn ? MarkType.Cross : MarkType.Nought;
+
+            button.Content = mPlayer1Turn ? "X" : "O";
+
+            button.Foreground = mPlayer1Turn ? Brushes.Red : Brushes.Blue;
+
+            mPlayer1Turn = !mPlayer1Turn;
+
+            CheckForWinner();
+        }
+
+        private void CheckForWinner()
+        {
+            #region Horizontal Wins
+            if (mResults[0] != MarkType.Free && mResults[0] == mResults[1] && mResults[1] == mResults[2])
+            {
+                mGameEnded = true;
+
+                Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Aqua;
+            }
+
+            if (mResults[3] != MarkType.Free && mResults[3] == mResults[4] && mResults[4] == mResults[5])
+            {
+                mGameEnded = true;
+
+                Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Aqua;
+            }
+
+            if (mResults[6] != MarkType.Free && mResults[6] == mResults[7] && mResults[7] == mResults[8])
+            {
+                mGameEnded = true;
+
+                Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Aqua;
+            }
+            #endregion
+
+            #region Vertical Wins
+            if (mResults[0] != MarkType.Free && mResults[0] == mResults[3] && mResults[3] == mResults[6])
+            {
+                mGameEnded = true;
+
+                Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Aqua;
+            }
+
+            if (mResults[1] != MarkType.Free && mResults[1] == mResults[4] && mResults[4] == mResults[7])
+            {
+                mGameEnded = true;
+
+                Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Aqua;
+            }
+
+            if (mResults[2] != MarkType.Free && mResults[2] == mResults[5] && mResults[5] == mResults[8])
+            {
+                mGameEnded = true;
+
+                Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Aqua;
+            }
+            #endregion
+
+            #region Diagonal Wins
+            if (mResults[0] != MarkType.Free && mResults[0] == mResults[4] && mResults[4] == mResults[8])
+            {
+                mGameEnded = true;
+
+                Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Aqua;
+            }
+
+            if (mResults[2] != MarkType.Free && mResults[2] == mResults[4] && mResults[4] == mResults[6])
+            {
+                mGameEnded = true;
+
+                Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Aqua;
+            }
+            #endregion
+
+            #region No Wins
+            if (!mResults.Any(f => f == MarkType.Free) && !mGameEnded)
+            {
+                mGameEnded = true;
+
+                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                {
+                    button.Background = Brushes.Orange;
+                });
+            }
+            #endregion
         }
     }
 }
